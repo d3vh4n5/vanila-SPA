2b039150587f70d34f83cfcc0f1b69ec99723fbb
Juan Basgall <juanangelbasgall@hotmail.com>
2024-05-09 16:57:23 -0300

    minor changes

--- assets/js/utils/fetchHtml.js
+++ assets/js/utils/fetchHtml.js
@@ -1,11 +1,31 @@
 export async function fetchHtml(path, callback) {
     try{
+        const app = document.querySelector('main')
+        app. innerHTML = 'Cargando...'
         const response = await fetch(path)
         const html = await response.text()
-        document.querySelector('main').innerHTML = html
+        app.innerHTML = html
         if (typeof callback === 'function') {
             callback(); 
         }
+    } catch (e){
+        console.error(e)
+        document.querySelector('main').innerHTML = "Failed to fetch: " + path;
+    }
+}
+
+export async function delayedFetchHtml(path, callback) {
+    try{
+        const app = document.querySelector('main')
+        app. innerHTML = 'Cargando...'
+        const response = await fetch(path)
+        const html = await response.text()
+        setTimeout(()=>{
+            app.innerHTML = html
+            if (typeof callback === 'function') {
+                callback(); 
+            }
+        }, 300)
     } catch (e){
         console.error(e)
         document.querySelector('main').innerHTML = "Failed to fetch: " + path;
